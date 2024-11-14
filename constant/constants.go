9b25ebbdeeac7470f5ba0754344323ccf1028820
VishalYadavOodles <ankit@ankit.mishra>
2024-11-14 18:20:56 +0530

    initial code

---
+++ constant/constants.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package constant
+
+const (
+	AppName                = "apm"
+	CoreAlias              = "ixAnkit/cryft-plugins-core"
+	CoreURL                = "https://github.com/ixAnkit/cryft-plugins-core.git"
+	CoreBranch             = "master"
+	QualifiedNameDelimiter = ":"
+	AliasDelimiter         = "/"
+	DefaultNetwork         = "testnet"
+)
